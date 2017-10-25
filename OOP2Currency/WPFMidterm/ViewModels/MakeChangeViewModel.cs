ce58c85b984292dbf9592f8877838b1b6d24e687
Exmintor <robertsmbj@gmail.com>
2017-10-24 21:11:49 -0500

    Adds Midterm Project
    Adds midterm project to the solution and sets up initial conditions for solving the assignment. (Also sets the DisplayMemberPath for the Listbox in the old project).

---
+++ OOP2Currency/WPFMidterm/ViewModels/MakeChangeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using CurrencyLibrary.Interfaces;
+using CurrencyLibrary.USCurrency;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMidterm.ViewModels
+{
+    public class MakeChangeViewModel : BaseViewModel
+    {
+        private USCurrencyRepo repository;
+        public ObservableCollection<ICoin> Coins
+        {
+            get
+            {
+                return new ObservableCollection<ICoin>(repository.Coins);
+            }
+        }
+        public MakeChangeViewModel(USCurrencyRepo repo)
+        {
+            this.repository = repo;
+        }
+
+        public void MakeChange(double amount)
+        {
+            repository.MakeChange(amount);
+            RaisePropertyChangedEvent("Coins");
+        }
+    }
+}
---
+++ OOP2Currency/WPFMidterm/ViewModels/RepoViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CurrencyLibrary.Interfaces;
+using CurrencyLibrary.USCurrency;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMidterm.ViewModels
+{
+    public class RepoViewModel : BaseViewModel
+    {
+        USCurrencyRepo repository;
+
+        public double TotalValue
+        {
+            get
+            {
+                return repository.TotalValue();
+            }
+        }
+
+        public RepoViewModel(USCurrencyRepo repo)
+        {
+            this.repository = repo;
+        }
+
+        public void AddCoins(USCoin coin)
+        {
+            repository.AddCoin(coin);
+            RaisePropertyChangedEvent("TotalValue");
+        }
+    }
+}
