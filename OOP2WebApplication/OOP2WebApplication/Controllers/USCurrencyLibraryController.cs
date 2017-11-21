fac29642440e7f9c641d66efb13e6e5d8abc0275
ROBERT-PC\Robert <robertsmbj@gmail.com>
2017-11-21 01:10:53 -0600

    Adds currency stuff to MVC
    Adds Currency library and creates a simple controller to Make Change from an amount.

---
+++ OOP2WebApplication/OOP2WebApplication/Controllers/USCurrencyLibraryController.cs
@@ -0,0 +1,41 @@
+﻿using CurrencyLibrary.USCurrency;
+using CurrencyLibrary;
+using CurrencyLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OOP2WebApplication.Controllers
+{
+    public class USCurrencyLibraryController : Controller
+    {
+        private USCurrencyRepo usCurrencyRepo;
+        public USCurrencyRepo UsCurrencyRepo
+        {
+            get
+            {
+                if (usCurrencyRepo == null)
+                {
+                    usCurrencyRepo = new USCurrencyRepo();
+                }
+                return usCurrencyRepo;
+            }
+            set
+            {
+                usCurrencyRepo = value;
+            }
+        }
+        // GET: USCurrencyLibrary
+        public ActionResult Index(double? amount)
+        {
+            if(amount == null)
+            {
+                return View(UsCurrencyRepo);
+            }
+            UsCurrencyRepo = (USCurrencyRepo)UsCurrencyRepo.MakeChange((double)amount);
+            return View(UsCurrencyRepo);
+        }
+    }
+}
